300128cda0f41b3638f32f11d94deab0c9ce5a9a
Jo Diekmann <jodi@dim3.de>
2024-01-23 17:11:21 +0100

    model and faker

---
+++ database/factories/teacherFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\teacher>
+ */
+class teacherFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->text(10),
+            'status' => $this->faker->randomElement(['new', 'est', 'ret'])
+
+        ];
+    }
+}
